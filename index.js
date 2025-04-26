280d876d3338eb8b286716346210f11685a2f8f8
andrjre <andrjre@gmail.com>
2025-04-26 21:47:32 +0100

    Working on title hover effect.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
+let title = document.getElementById("andreTitle");
+let titleText = document.getElementById("andreTitle").textContent;
 
+let splitTitle = []
+splitTitle = titleText.split("")
+console.log(splitTitle)
+
+splitTitle[0].onclick = function(){
+    console.log("helloooo")
+}
+
+title.onclick = function(){
+    console.log("gello")
+}
