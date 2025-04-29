22502bb7b69d882ecb86e51ff1e177a7bb2af230
andrjre <andrjre@gmail.com>
2025-04-29 20:44:29 +0100

    Messing around.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 let title = document.getElementById("andreTitle");
 let titleText = document.getElementById("andreTitle").textContent;
-
+let aText = document.querySelector("a")
 
 title.onclick = function(){
     title.classList.add("rainbow")
 }
 
 
-let button = document.querySelector(".button")
+let button = document.querySelector("#button")
 let git = document.getElementById("git")
 
+button.addEventListener("mouseover",function(){console.log("gello")},
+    function(){aText.style.color = "black"},
+    
+)
 
 git.addEventListener("mouseover", function(){console.log("gello");})
