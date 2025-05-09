015a050629bca77c540f1ff0556b3fdfd1177294
andrjre <andrjre@gmail.com>
2025-05-09 19:42:35 +0100

    Confirm submit.

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 let title = document.getElementById("andreTitle");
 let titleText = document.getElementById("andreTitle").textContent;
 let aText = document.querySelector("a")
+let submit = document.querySelector("#submit")
+let footer = document.querySelector("#footer")
+
+submit.addEventListener("click", 
+    function()
+    {console.log("Form Submitted!")
+    let confirm = document.createElement("div")
+    confirm.id = "confirm"
+    confirm.textContent = "confirmed!"
+    footer.append(confirm)
+    submit.disabled = true
+    }
+)
+    
+
 
 title.onclick = function(){
     title.classList.add("rainbow")
